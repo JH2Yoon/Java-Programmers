8cf5bebf6c8dbe0b1dfb1a3332b95329af3a6716
JH1Yoon <wlgus4589@naver.com>
2024-09-01 23:13:36 +0900

    98단계 완료

---
+++ solutions/Java98.java
@@ -0,0 +1,34 @@
+// 98. 뒤에 있는 큰 수 찾기
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Stack<Integer> stack = new Stack<>();
+        
+        
+        int[] answer = new int[numbers.length];
+
+
+        stack.push(0);
+
+    
+        for (int i = 1; i < numbers.length; i++) 
+        {
+        
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) 
+            {
+            
+                answer[stack.pop()] = numbers[i];
+            }
+            stack.push(i);
+        }
+    
+        while (!stack.isEmpty()) 
+        {
+            answer[stack.pop()] = -1;
+        }
+
+        return answer;
+    }
+}
