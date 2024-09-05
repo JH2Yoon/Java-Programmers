d8e5c0bc094fd4f7dec8609eb367bd38619b3977
JH1Yoon <wlgus4589@naver.com>
2024-09-05 09:16:17 +0900

    102단계 완료

---
+++ solutions/Java102.java
@@ -0,0 +1,50 @@
+// 102. 다리를 지나는 트럭
+
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        
+        Queue<Integer> queue = new LinkedList<>();
+        int sum = 0;
+        int time = 0;
+        
+        for (int i=0; i<truck_weights.length; i++)
+        {
+            int truck = truck_weights[i];
+            
+            while(true)
+            {
+                if (queue.isEmpty())
+                {
+                    queue.offer(truck);
+                    sum += truck;
+                    time++;
+                    break;
+                }
+                else if (queue.size() == bridge_length)
+                {
+                    sum -= queue.poll();
+                }
+                else {
+                    if (sum + truck <= weight)
+                    {
+                        queue.offer(truck);
+                        time++;
+                        sum += truck;
+                        break;
+                    }
+                    else
+                    {
+                        queue.add(0);
+                        time++;
+                    }
+                }
+            }
+        }
+        
+        
+        
+        return time + bridge_length;
+    }
+}
